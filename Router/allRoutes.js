bfc01bcd72ba7bb92783077f02545166eaaadd3e
world_vcg <jbkihhv@gmail.com>
2025-08-02 02:15:20 +0200

    page contact fini

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,8 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/",  "Accueil", "/pages/home.html"),
     new Route("/covoiturages", "Covoiturages", "/pages/covoiturages.html"),
+    new Route("/trajetdetail", "trajet-detail" ,"/pages/trajet-detail.html"),
+    new Route("/contact", "contact", "/pages/contact.html"),
 ];
 
 
